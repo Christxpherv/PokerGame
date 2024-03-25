1506a830c8dcbc9c1aa390530b8a1911c10a5588
Christopher Valle <christxpherv@gmail.com>
2024-03-25 10:28:34 -0500

    added a get_value method to the rspec to test a method intended to return the string value from a constants array

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -15,4 +15,11 @@ RSpec.describe Card do
       expect { Card.new(:Hearts, :Eleven) }.to raise_error("invalid card, please verify the suit and value.")
     end
   end
+
+  # get the value of the card as a string
+  describe "#get_value" do
+    it "returns string representation of value" do
+      expect(card.get_value).to eq("10")
+    end
+  end
 end
