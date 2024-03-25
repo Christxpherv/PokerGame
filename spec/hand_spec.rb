714a4ab03c0e0e74f8e3505330baf7fcaab0a94b
Christopher Valle <christxpherv@gmail.com>
2024-03-25 13:36:44 -0500

    finished creating the mock hands for the rspec file

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -15,4 +15,17 @@ describe Hand do
   let(:five_of_s) { double("five_of_s", :get_integer => 5, :get_suit => :spades)}
   let(:six_of_s) { double("six_of_s", :get_integer => 6, :get_suit => :spades)}
   let(:seven_of_s) { double("seven_of_s", :get_integer => 7, :get_suit => :spades)}
+
+  # finish creating the mock hands
+  let(:high_card) {[two_of_h, three_of_h, four_of_s, five_of_s, seven_of_s]}
+  let(:pair) {[two_of_h, two_of_c, three_of_s, four_of_s, five_of_s]}
+  let(:two_pair) {[two_of_h, two_of_d, three_of_h, three_of_s, seven_of_s]}
+  let(:three_of_a_kind) {[two_of_h, two_of_d, two_of_c, three_of_h, four_of_s]}
+  let(:four_of_a_kind) {[two_of_h, two_of_d, two_of_c, two_of_s, four_of_s]}
+  let(:straight) {[three_of_h, four_of_s, five_of_s, six_of_s, seven_of_s]}
+  let(:flush) {[two_of_s, four_of_s, five_of_s, six_of_s, seven_of_s]}
+  let(:full_house) {[two_of_s, two_of_h, two_of_d, three_of_h, three_of_s]}
+  let(:straight_flush) {[three_of_s, four_of_s, five_of_s, six_of_s, seven_of_s]}
+
+
 end
