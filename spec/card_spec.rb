d1fa6bac5c5ba03d2a28562f713a5338e5f61d06
Christopher Valle <christxpherv@gmail.com>
2024-03-25 10:26:00 -0500

    updated the spec file to unclude testing for constants and for error handling

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,12 +2,17 @@ require 'card'
 
 RSpec.describe Card do
 
-  subject(:card) { Card.new("Hearts", "10") }
+  subject(:card) { Card.new(:Hearts, :Ten) }
 
   describe '#initialize' do
     it 'creates a card with a suit and a value' do
-      expect(card.suit).to eq("Hearts")
-      expect(card.value).to eq("10")
+      expect(card.suit).to eq(:Hearts)
+      expect(card.value).to eq(:Ten)
+    end
+
+    # test if the suit or value is invalid, it returns an error message
+    it "should return an error if the suit or value is invalid" do
+      expect { Card.new(:Hearts, :Eleven) }.to raise_error("invalid card, please verify the suit and value.")
     end
   end
 end
