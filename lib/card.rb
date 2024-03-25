98dcf59bc56cf8d50525abdf1072d7cdd85a9712
Christopher Valle <christxpherv@gmail.com>
2024-03-25 10:35:01 -0500

    created the hash constants for the symbols, string values, and integer values. spec file still red

--- lib/card.rb
+++ lib/card.rb
@@ -1,5 +1,20 @@
 class Card
 
+  # create constant hash for symbols, string values, and integer values
+  SYMBOLS = {:Clubs => "♣", :Diamonds => "♦", :Hearts => "♥", :Spades => "♠" }
+
+  STRINGS = {
+    :Two => "2", :Three => "3", :Four  => "4", :Five  => "5", :Six   => "6",
+    :Seven => "7", :Eight => "8", :Nine  => "9", :Ten   => "10", :Jack  => "J",
+    :Queen => "Q", :King  => "K", :Ace   => "A"
+  }
+
+  VALUES = {
+    :Two => 2, :Three => 3, :Four  => 4, :Five  => 5, :Six   => 6,
+    :Seven => 7, :Eight => 8, :Nine  => 9, :Ten   => 10,
+    :Jack  => 11, :Queen => 12, :King  => 13, :Ace   => 14
+  }
+
   # create getter methods for instance variables, suit and value
   attr_reader :suit, :value
 
