6d763b16c459328aceb3431ed0d6fb71f01b0734
Christopher Valle <christxpherv@gmail.com>
2024-03-25 19:41:09 -0500

    started rspec file for the player class; created testing for the fold method which should switch a boolean attribute to true. initially started this in the wrong file  (correction)

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,15 @@
 require 'player'
+
+describe Player do
+  # create a player instance with a bankroll of 400 and a pot of 100
+  subject(:player) { Player.new(400, 100)}
+
+  describe "#fold" do
+    it "should set the player to folded" do
+      # call the fold method on the player
+      player.fold
+      # expect the player to be folded; predicate matcher expects boolean attribute to be true
+      expect(player).to be_folded
+    end
+  end
+end
